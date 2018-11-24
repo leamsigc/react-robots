52bef585fe3f74f204efae1be03fc78e2765298b
leamsigc <leamsigc@hotmail.com>
2018-11-24 16:22:43 -0500

    Updates

---
+++ precache-manifest.d572361c060b43fa58fa5aca5bbbd0f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cfe9cd962f4d080bb8c",
+    "url": "/static/css/main.eeca2173.chunk.css"
+  },
+  {
+    "revision": "6cfe9cd962f4d080bb8c",
+    "url": "/static/js/main.6cfe9cd9.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee574618a2ba18367bd977dcc3219f10",
+    "url": "/index.html"
+  }
+];
